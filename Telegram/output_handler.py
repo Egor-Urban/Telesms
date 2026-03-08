3d19b3c07edaddf43e5b7f5e4ce8584afbb77f45
Egor-Urban <egor.zhurban@gmail.com>
2026-03-09 01:07:30 +0300

    Modified captions and file types handling

--- Telegram/output_handler.py
+++ Telegram/output_handler.py
@@ -1,8 +1,11 @@
 from hydrogram import Client, filters
 
 
-
 def output(app, log):
+
+    def get_caption(message):
+        return message.caption if message.caption else ""
+
     @app.on_message(filters.private)
     async def log_message(client, message):
         user = message.from_user
@@ -12,13 +15,25 @@ def output(app, log):
             log.info(f"Message from [{username}::{user.id}] ({message.id}): {message.text}")
 
         elif message.photo:
-            log.info(f"Photo from [{username}::{user.id}] ({message.id})")
+            log.info(f"Photo from [{username}::{user.id}] ({message.id}) caption: {get_caption(message)}")
 
         elif message.video:
-            log.info(f"Video from [{username}::{user.id}] ({message.id})")
+            log.info(f"Video from [{username}::{user.id}] ({message.id}) caption: {get_caption(message)}")
+
+        elif message.animation:  # gif
+            log.info(f"GIF from [{username}::{user.id}] ({message.id}) caption: {get_caption(message)}")
 
         elif message.document:
-            log.info(f"File from [{username}::{user.id}]: {message.document.file_name} ({message.id})")
+            log.info(
+                f"File from [{username}::{user.id}] ({message.id}): "
+                f"{message.document.file_name} caption: {get_caption(message)}"
+            )
+
+        elif message.sticker:
+            log.info(
+                f"Sticker from [{username}::{user.id}] ({message.id}) "
+                f"emoji: {message.sticker.emoji}"
+            )
 
         else:
             log.info(f"Other message from [{username}::{user.id}] ({message.id})")
@@ -30,16 +45,35 @@ def output(app, log):
         username = f"@{user.username}" if user and user.username else "no_username"
 
         if message.text:
-            log.info(f"EDITED message from [{username}::{user.id}]: {message.text} ({message.id})")
+            log.info(
+                f"EDITED message from [{username}::{user.id}] ({message.id}): {message.text}"
+            )
 
         elif message.photo:
-            log.info(f"EDITED photo from [{username}::{user.id}] ({message.id})")
+            log.info(
+                f"EDITED photo from [{username}::{user.id}] ({message.id}) "
+                f"new caption: {get_caption(message)}"
+            )
 
         elif message.video:
-            log.info(f"EDITED video from [{username}::{user.id}] ({message.id})")
+            log.info(
+                f"EDITED video from [{username}::{user.id}] ({message.id}) "
+                f"new caption: {get_caption(message)}"
+            )
+
+        elif message.animation:
+            log.info(
+                f"EDITED GIF from [{username}::{user.id}] ({message.id}) "
+                f"new caption: {get_caption(message)}"
+            )
 
         elif message.document:
-            log.info(f"EDITED file from [{username}::{user.id}]: {message.document.file_name} ({message.id})")
+            log.info(
+                f"EDITED file from [{username}::{user.id}] ({message.id}) "
+                f"{message.document.file_name} new caption: {get_caption(message)}"
+            )
 
         else:
-            log.info(f"EDITED other message from [{username}::{user.id}] ({message.id})")
+            log.info(
+                f"EDITED other message from [{username}::{user.id}] ({message.id})"
+            )
